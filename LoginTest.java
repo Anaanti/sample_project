6eabc224373228152c0e704aa018f06c77cbaa7e
Anaanti Yati <yatianaanti@gmail.com>
2025-03-11 22:08:13 +0530

    Changes to file

--- LoginTest.java
+++ LoginTest.java
@@ -5,16 +5,12 @@ import org.testng.annotations.Test;
 public class LoginTest {
     @Test
     public void openGoogle() {
-        // Set path to your ChromeDriver
         System.setProperty("webdriver.chrome.driver", "C:/path/to/chromedriver.exe");
 
-        // Initialize WebDriver
         WebDriver driver = new ChromeDriver();
 
-        // Open Google
         driver.get("https://www.google.com");
 
-        // Close the browser
         driver.quit();
     }
 }
